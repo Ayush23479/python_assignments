6a1b487ae613a5ed608d3180f986633ff2ff7f3a
Ayush23479 <ayush.tmg845@gmail.com>
2025-05-14 06:30:57 +0545

    Add files via upload

---
+++ basic_projects/1st.py
@@ -0,0 +1,12 @@
+
+# Basic Mathematical Operations
+
+num1 = int(input("Enter a number : "))
+num2 = int(input("Enter a number : "))
+
+print("Addition : ",num1 + num2)
+print("Subtraction : ", num1-num2)
+print("Multiplication : " , num1*num2)
+print("Division : ",num1/num2)
+
+
---
+++ basic_projects/2nd.py
@@ -0,0 +1,7 @@
+
+# Personalized Greeting
+
+fname = input("Enter your first name : ")
+lname = input("Enter your last name : ")
+
+print("Hello,",fname ,lname,"!Welcome to the python program.")
